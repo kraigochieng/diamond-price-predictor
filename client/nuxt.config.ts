9f022444ebd28efd3b26afe25223379f1fd71c3c
kraigochieng <kraigochieng@gmail.com>
2025-09-30 20:34:55 +0300

    fix: make default api base to be localhost

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
 	modules: ["@nuxt/ui"],
 	runtimeConfig: {
 		public: {
-			apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://api.example.com',
+			apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000',
 		}
 	},
 	vite: {
